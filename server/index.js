b013788c66048161973493797d5573f108c39073
xiedacon <xiedacon@qq.com>
2018-04-06 20:29:33 +0800

    feat: auto open browser

--- server/index.js
+++ server/index.js
@@ -3,11 +3,18 @@
 const server = require('server')
 const { get } = server.router
 const { redirect } = server.reply
+const opn = require('opn')
 
-server({ port: 3000 },
+let port = 3000
+let url = `http://localhost:${port}`
+
+server({ port },
   require('./cors'),
   get('/', ctx => redirect('/index.html')),
   require('./swagger'),
   require('../api'),
   require('./static')
 )
+
+console.log(`Visit: ${url}`)
+opn(url)
